3450ea761bedf2f836992361580e6668c57b7bf9
XDWOLF <62610377+nirajxhrestha@users.noreply.github.com>
2021-12-12 14:35:57 +0545

    Add files via upload

--- core/bot.php
+++ core/bot.php
@@ -1,6 +1,6 @@
 <?php
 // Initialize the session
-session_start();
+#session_start();
  
 // Include config file
 require_once "./connection/connection.php";
@@ -26,17 +26,68 @@ require_once "./connection/connection.php";
                 </div>
                 <div class="msg-header">
                     <p>Hello there, how can I help you?</p>
+
+                    <?php
+                        if(array_key_exists('button1', $_POST)) {
+                            button1();
+                        }
+                        else if(array_key_exists('button2', $_POST)) {
+                            button2();
+                        }
+                        else if(array_key_exists('button3', $_POST)) {
+                            button3();
+                        }
+                        function button1() {
+                            echo "Can i know if any doctors are available?";
+                        }
+                        function button2() {
+                            echo "Can I know your location?";
+                        }
+                        function button3() {
+                            echo "What is your opening hour?";
+                        }
+                    ?>
+                    <br>
+                    <form method="post">
+                        <button type="submit" name="button1" id="button1">Appointment</button>
+                        <button type="submit" name="button2" id="button2">Location</button>
+                        <button type="submit" name="button3" id="button3">Opening Hour</button>
+                    </form>
                 </div>
             </div>
         </div>
-                
         <div class="typing-field">
             <div class="input-data">
-                <input id="data" type="text" placeholder="Type something here.." required>
+                <input id="data" type="text" placeholder="Type your message here.." required>
                 <button id="send-btn">Send</button>
             </div>
         </div>
     </div>
+
+
+    <script>
+        $(document).ready(function(){
+            $("#showData").on("click", function(){
+                $value = $("#button1").val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+                $(".form").append($msg);
+                $("#button1").val('');
+                
+                // start ajax code
+                $.ajax({
+                    url: './core/message.php',
+                    type: 'POST',
+                    data: 'text='Appointment,
+                    success: function(result){
+                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        $(".form").append($replay);
+                        // when chat goes down the scroll bar automatically comes to the bottom
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                });
+            });
+        });
+    </script>
 
     <script>
         $(document).ready(function(){
