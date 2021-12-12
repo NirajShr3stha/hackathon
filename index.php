0d2a2a0e5b0f0577b05d7580b67aa66df7502862
XDWOLF <62610377+nirajxhrestha@users.noreply.github.com>
2021-12-12 14:35:37 +0545

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
+<?php
+#====================================#
+session_start();
+#echo $_SESSION['login'];
+include('connection/connection.php') 
+#====================================#
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<!--=====================================================-->
 	<title>Hospital</title>
-	<!--=====================================================-->
-	<meta charset="utf-8">
-	<meta name="description" content="">
-	<!--=====================================================-->
-	<meta name="keywords" content="">
-
 </head>
+
 <body>
 	<div class="wrapper">
 			<tr>
--- menu.php
+++ menu.php
@@ -1,3 +1,4 @@
 <div align="center" class="navmenu">
-| <a href="user/login.php">LOGIN</a> |
+| <a href="user/login.php">Login</a> |
+| <a href="index.php?page=user/logout">Logout</a> |
 </div>
